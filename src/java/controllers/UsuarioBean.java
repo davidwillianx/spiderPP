b7eac187cfebf335f593d7410e66b0b20d051806
smp <smp@smp-QBEX-G31T-M7>
2014-02-28 17:12:34 -0300

    estrutura de diretorios do interface

--- src/java/controllers/UsuarioBean.java
+++ src/java/controllers/UsuarioBean.java
@@ -5,9 +5,15 @@
  */
 
 package controllers;
+import java.util.Properties;
 import javax.faces.bean.ManagedBean;
 import models.persistence.UsuarioDao;
 import models.entities.Usuario;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+
+
 
 /**
  *
@@ -35,7 +41,41 @@ public class UsuarioBean {
     
     public void save(Usuario usuario)
     {
-        this.usuarioDao.save(usuario);
+//        this.usuarioDao.save(usuario);
+        
+        Properties properties = new Properties();
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.setProperty("mail.host", "smtp.ufpa.br");
+        properties.setProperty("mail.user", "david.lopes@icen.ufpa.br");
+        properties.setProperty("mail.password", "spiderteste");
+        
+        
+        
+        try {
+            Session sessionMail =  Session.getDefaultInstance(properties,null);
+            Transport transportMail = sessionMail.getTransport();
+            
+            MimeMessage mimeMessage = new MimeMessage(sessionMail);
+            mimeMessage.setSubject("testando envio de mensagens");
+            mimeMessage.setContent("estamos testando essa bagaça", "text/plain");
+            mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress("davidcbsi@gmail.com"));
+            
+            transportMail.connect();
+            transportMail.sendMessage(mimeMessage, mimeMessage.getRecipients(Message.RecipientType.TO));
+            transportMail.close();
+            System.err.println("Foi enviado!");
+            
+        } catch (Exception e) {
+            System.out.println("error: "+ e.getMessage());
+            e.printStackTrace();       
+        }
+ 
+        
+            
+         
+        
+        
+        
+        
     }
-    
 }
