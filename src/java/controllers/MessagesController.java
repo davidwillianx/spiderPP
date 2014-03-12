8a818f03cda118512b46caf7435ac6eac9482ca0
murakamibruno <murakami.bruno@gmail.com>
2014-03-12 16:54:24 -0300

    Spider Commit

---
+++ src/java/controllers/MessagesController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controllers;
+
+    import javax.faces.application.FacesMessage;
+    import javax.faces.bean.ManagedBean;
+    import javax.faces.context.FacesContext;
+    import javax.faces.event.ActionEvent;
+
+
+    /**
+     *
+     * @author Bruno
+     */
+    @ManagedBean
+    public class MessagesController {
+
+        public void addInfo(ActionEvent actionEvent)
+        {
+            addMessage("Para efetivar seu cadastro, acesse o link de ativação que foi enviado ao seu e-mail.");
+        }
+
+        public void addMessage(String summary)
+        {
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,summary,null);
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }   
+    }
+
+
