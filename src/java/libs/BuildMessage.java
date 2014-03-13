d82e9938dcd197d50e49824451447af8ab6f66aa
murakamibruno <murakami.bruno@gmail.com>
2014-03-13 19:13:34 -0300

    Spider Commit

---
+++ src/java/libs/BuildMessage.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package libs;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+
+public class BuildMessage {
+    
+    public void addInfo()
+    {    
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Aviso:", "Acesse seu email para validar o cadastro."));
+    }
+    
+    public void addError()
+    {
+        FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_ERROR,"Erro:","E-mail já cadastrado, tente com outro e-mail."));
+    }
+}
