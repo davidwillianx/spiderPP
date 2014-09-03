3f11ca13de91ab11b2c30d5dff0c872c3737e9f8
smartphonnee <smartphonnee@smartphonne-elementary>
2014-09-03 00:14:31 -0300

    ajuste para inclusão da estoria do usuario

--- src/java/socket/GameSocket.java
+++ src/java/socket/GameSocket.java
@@ -12,18 +12,18 @@ import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.inject.Inject;
+import javax.inject.Inject; 
 import javax.json.Json;
-import javax.websocket.EncodeException;
+import javax.websocket.EncodeException; 
 import javax.websocket.OnClose; 
-import javax.websocket.OnMessage;
+import javax.websocket.OnMessage; 
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import models.ejb.MensagemBean;
-import models.entities.Mensagem;
-
+import models.entities.Mensagem; 
+ 
 /** 
  *
  * @author smartphonnee
@@ -41,13 +41,13 @@ public class GameSocket implements Serializable {
 
     @OnOpen
     public void onOpen(Session session, @PathParam("room") String room, @PathParam("perfil") String perfil) {
-        try {
+        try { 
             participant = new Participant(room, perfil, session);
             if (participant.isScrumMaster()) {
                 Game game = new Game(participant); 
                 games.add(game);  
                 this.loadPreviousMessage(session);
-            } else {
+            } else { 
                 Game game = this.getGameByProject(participant.getIdProjeto());
                 if (game instanceof Game) {
                     if (game.isOpen()) {
@@ -58,31 +58,31 @@ public class GameSocket implements Serializable {
                     this.loadPreviousMessage(session);
                 } else {
                     this.OnClose(session);
-                }
-            }
+                } 
+            } 
         } catch (IOException | EncodeException e) {
-            //Call OnError
+            //Call OnError 
             System.err.println("Do something" + e.getMessage());
-        } 
-    }   
-    
+        }  
+    }    
+     
     //BAM BAM BAM REFACTORY DETECTED PAY ATTENTION SR 
     @OnMessage
     public void OnMessage(Session session, Message message) {
-        try {
-
+        try { 
+  
               Game game  = this.getGame(session);
               
             if ("message".equals(message.getJson().getString("type"))) {
                 mensagemBean.save(new ChatMessage(message.getJson()));
                 game.sendBroadcastMessageWithoutSender(session, message);
-            }   
-  
+            }         
+     
             if ("gameStart".equals(message.getJson().getString("type"))) { 
                 game.start();
                 game.sendBroadcastMessage(session,message); 
-            }
-            
+            } 
+             
             if("cardSelected".equals(message.getJson().getString("type")))
             {
                 //TODO pensando sobre sim ou não de armazenar esta informação no servidor
@@ -90,21 +90,27 @@ public class GameSocket implements Serializable {
                 //game.addCard(message, session);
                 game.sendBroadcastMessageWithoutSender(session, message);
             }
-               
+                 
             if("gameUnlock".equals(message.getJson().getString("type")))
             {
                 if(game.getParticipant(session).isScrumMaster()){
                         game.closeRound();
                         game.sendBroadcastMessageWithoutSender(session, message);
-                }
+                }   
             }
+             
+            if("story".equals(message.getJson().getString("type")))
+            { 
+                if(game.getParticipant(session).isScrumMaster())
+                    game.sendBroadcastMessageWithoutSender(session, message);
+            }   
 
         } catch (Exception e) {
             //Call on error and close connection
         } 
-    }
-
-    @OnClose
+    }  
+  
+    @OnClose   
     public void OnClose(Session session) {
         try {
             //Fechar a conexão/Remover do game/ redirecionar/
@@ -115,12 +121,12 @@ public class GameSocket implements Serializable {
             Logger.getLogger(GameSocket.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
+  
     public void loadPreviousMessage(Session session) throws IOException, EncodeException {
 
         List<Mensagem> messages = mensagemBean.getMensagensByProjeto(participant.getIdProjeto());
         for (Mensagem mensagem : messages) {
-            
+             
                  Message message = new Message(Json.createObjectBuilder().add("author", mensagem.getUsuario().getNome())
                     .add("message", mensagem.getTexto())
                     .add("idProjeto", mensagem.getProjeto().getId())
@@ -129,7 +135,7 @@ public class GameSocket implements Serializable {
                     .build()); 
             
             session.getBasicRemote().sendObject(message);
-        }
+        } 
     }
 
     private Game getGame(Session userSession) {
